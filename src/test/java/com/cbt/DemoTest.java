30c71e88334a1c11b638310008232e7f27556867
aturanov <abdu.turon@gmail.com>
2016-12-13 15:24:10 -0500

    printing another message

--- src/test/java/com/cbt/DemoTest.java
+++ src/test/java/com/cbt/DemoTest.java
@@ -4,6 +4,7 @@ public class DemoTest {
     public static void main (String [] args){
     	
     	System.out.println("Hello Snow");
+    	System.out.println("PRINTING ANOTHER MESSAGE ");
     	
     }
 }
